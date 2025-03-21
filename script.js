2e9c0f5255376508e4257e9078c2974ca4f6430c
NicolyFernandees <nycolysennaa@gmail.com>
2025-03-21 20:35:00 -0300

    LuzApagada

---
+++ script.js
@@ -0,0 +1,9 @@
+function ligar(){
+    let luz = document.getElementById('luz');
+    luz.src = "img/jujutsu-kaisen-ep-4.jpg";
+}
+
+function desligar(){
+    let luz = document.getElementById('luz');
+    luz.src = "img/3-3-1536x864.png";
+}
